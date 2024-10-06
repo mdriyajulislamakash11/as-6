6b37800e484985300a5e6c975b706ed4b9508a3f
akash ahmed <hafejmohammadakash@gmail..com>
2024-10-07 03:07:02 +0600

    project added

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,11 @@
 
 // load categories button
 const loadCategories = () => {
-    // fethe
     fetch(`https://openapi.programming-hero.com/api/peddy/categories`)
         .then(response => response.json())
         .then(data => displayCategories(data.categories))
         .catch(error => console.log(error))
 };
-
-// {
-//     "petId": 1,
-//     "breed": "Golden Retriever",
-//     "category": "Dog",
-//     "date_of_birth": "2023-01-15",
-//     "price": 1200,
-//     "image": "https://i.ibb.co.com/p0w744T/pet-1.jpg",
-//     "gender": "Male",
-//     "pet_details": "This friendly male Golden Retriever is energetic and loyal, making him a perfect companion for families. Born on January 15, 2023, he enjoys playing outdoors and is especially great with children. Fully vaccinated, he's ready to join your family and bring endless joy. Priced at $1200, he offers love, loyalty, and a lively spirit for those seeking a playful yet gentle dog.",
-//     "vaccinated_status": "Fully",
-//     "pet_name": "Sunny"
-// }
 
 // load cards 
 const loadCards = () => {
@@ -30,35 +16,52 @@ const loadCards = () => {
         .catch(error => console.log(error))
 };
 
+const loadCategoriesCard = (id) => {
+   
+
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
+        .then(res => res.json())
+        .then(data => displayCards(data.data))
+        .catch(error => console.log(error))
+
+
+    setTimeout( function () {    
+    }, 2000)
+}
+
 const displayCards = (card) => {
     const cardContainer = document.getElementById('card-container')
-    console.log(card)
-
+    cardContainer.innerHTML= "";
     card.forEach((cards) => {
-        console.log(cards)
         const cardDiv = document.createElement('div');
-        cardDiv.classList = "bg-red-500"
+        cardDiv.classList = "card bg-base-100 shadow-xl m-3 "
         cardDiv.innerHTML = `
 
-        <div class="card bg-base-100 w-96 shadow-xl">
-        <figure class="px-10 pt-10">
-            <img class="object-cover"
-            src= ${cards.image}
-            alt="Shoes"
-            class="rounded-xl" />
-        </figure>
-        <div class="card-body">
-            <div>
-                <h1>${cards.pet_name}</h1>
+        
+            <figure class="px-5 pt-5 h-[200px]">
+                <img
+                src= ${cards.image}
+                alt="Shoes"
+                class="rounded-xl h-full w-full object-cover" />
+            </figure>
+            <div class="px-5 pt-5">
+                <div> 
+                <h2 class="text-[20px] font-bold inter mb-2"> ${cards.pet_name}</h2>
+                    <p>  <i class="fa-solid fa-border-all"></i> Breed: ${cards.breed}</p>
+                    <p> <i class="fa-regular fa-calendar-days"></i> Birth: ${cards.date_of_birth}</p>
+                    <p> <i class="fa-solid fa-mercury"></i> Gender: ${cards.gender}</p>
+                    <p> <i class="fa-solid fa-dollar-sign"></i> Price: ${cards.price}$ </p>
+                </div>
 
-                <p >${cards.breed}, </p>
-                <p >${cards.date_of_birth}</p>
-                <p >${cards.gender}</p>
-                <p >${cards.price}</p>
+               <div class="grid grid-cols-2 items-center mt-3">
+                    <p class="btn text-[18px] font-bold"> <i class="fa-regular fa-thumbs-up "></i></p>
+                    <button class="btn text-[#0E7A81] text-[18px] font-bold">Adopt</button>
+                    <button class="btn text-[#0E7A81] text-[18px] font-bold col-span-2 mt-2">Details</button>
+                </div>
+
                 
             </div>
-        </div>
-        </div>
+        
         `;
         cardContainer.append(cardDiv)
     });
@@ -76,15 +79,20 @@ const displayCategories = (categorys) => {
         const div = document.createElement('div');
         div.classList = 'btn w-3/12  rounded-full h-[80px]'
         div.innerHTML = `
-        <div class="flex items-center text-2xl font-bold inter">
+        <button onclick="loadCategoriesCard('${item.category}')" class="flex items-center text-2xl font-bold inter">
              <img src="${item.category_icon}" icon" class="w-10">
             <p>${item.category}</p>
-        </div>
+        </button>
         
-        `
+        `;
+
         displayCategoriesButton.append(div);
     });
+
 };
+
+
+
 
 loadCategories();
 loadCards();
