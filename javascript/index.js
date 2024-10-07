3c09eb60ec856edefc669e3f70490dc1c44e8950
akash ahmed <hafejmohammadakash@gmail..com>
2024-10-07 11:51:31 +0600

    error section added

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,8 @@
 
 // load categories button
 const loadCategories = () => {
+    
+
     fetch(`https://openapi.programming-hero.com/api/peddy/categories`)
         .then(response => response.json())
         .then(data => displayCategories(data.categories))
@@ -17,21 +19,40 @@ const loadCards = () => {
 };
 
 const loadCategoriesCard = (id) => {
-   
+    
 
-    fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
+    
+        fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
         .then(res => res.json())
         .then(data => displayCards(data.data))
         .catch(error => console.log(error))
 
-
-    setTimeout( function () {    
-    }, 2000)
+    
 }
 
 const displayCards = (card) => {
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML= "";
+    
+    if(card.length == 0){
+        cardContainer.classList.remove('grid') 
+        cardContainer.innerHTML=`
+        
+        <div class="main-h-[400px] w-full flex flex-col gap-5 justify-center items-center text-center py-[100px]">
+            <img src="./images/error.webp" >
+            <p class="text-[32px] font-bold">No Information Available</p>
+            <p class="text-gray-400">It is a long established fact that a reader will be distracted by   the readable content of a page when looking at
+             <br>   its layout. The point of using Lorem Ipsum is that it has a.
+            </p>
+        </div>
+        
+        `;
+        return cardContainer;
+    }else{
+        cardContainer.classList.add('grid') 
+    }
+
+
     card.forEach((cards) => {
         const cardDiv = document.createElement('div');
         cardDiv.classList = "card bg-base-100 shadow-xl m-3 "
@@ -77,11 +98,11 @@ const displayCategories = (categorys) => {
         // console.log(item);
         // create button ctaegories
         const div = document.createElement('div');
-        div.classList = 'btn w-3/12  rounded-full h-[80px]'
+        div.classList = 'btn w-2/12 rounded-full h-[80px]'
         div.innerHTML = `
-        <button onclick="loadCategoriesCard('${item.category}')" class="flex items-center text-2xl font-bold inter">
+        <button onclick="loadCategoriesCard('${item.category}')" class="flex items-center text-2xl font-bold inter gap-5 my-6">
              <img src="${item.category_icon}" icon" class="w-10">
-            <p>${item.category}</p>
+            <p class="hidden md:block">${item.category}</p>
         </button>
         
         `;
